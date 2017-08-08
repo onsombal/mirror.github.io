0f47dd8c5ce7d7857c1303081e32a8d6ea664816
Jim Biggs <jimbiggs@Jims-MacBook-Pro.local>
2017-08-07 18:15:55 -0700

    First commit

---
+++ cms/.users.js
@@ -0,0 +1,11 @@
+{
+	users: [
+		{
+			username: 'admin',
+			tags: [],
+			salt: 'f014bf6d7ccbc7220bfd24a53b068cac',
+			hash: '850acd5953ae6da48033fccbc1984cd690e4a5b76149bc29900f50416aa4248e',
+			user_created_timestamp: 1502132927554
+		}
+	]
+}
---
+++ cms/undefined.js
@@ -0,0 +1,11 @@
+{
+	users: [
+		{
+			username: 'admin',
+			tags: [],
+			salt: '67dbcad6f34413659da5cd010a57f54e',
+			hash: 'ba0dad9a7f458cc87102f447d0d150b134baf14dd1361994254e79982fb276c6',
+			user_created_timestamp: 1502132800574
+		}
+	]
+}
